17f43475991a2d8860fb061b909668e61ad23df3
Alexey <alexey-sh@users.noreply.github.com>
2019-11-08 22:22:18 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -12,19 +12,5 @@ module.exports = {
     },
     node: {
         fs: 'empty'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env']
-                    }
-                }
-            }
-        ]
     }
 };
