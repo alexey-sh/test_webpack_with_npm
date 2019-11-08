c0f97a88f1ac19ed126d1cbe755a62efcae56a04
Alexey <alexey-sh@users.noreply.github.com>
2019-11-08 13:02:54 +0300

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+
+module.exports = {
+    entry: './main.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'asdLib', // public library name
+        libraryTarget: 'umd',
+        globalObject: 'this' // To make UMD build available on both browsers and Node.js
+    },
+    node: {
+        fs: 'empty'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        "presets": [
+                            [
+                                "@babel/preset-env",
+                                {
+                                    "targets": {
+                                        chrome: '40',
+                                        firefox: '52',
+                                        ie: '11',
+                                        safari: '9'
+                                    }
+                                }
+                            ]
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+};
