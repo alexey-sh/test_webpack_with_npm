a0b36698e88f1426aba1907d48329a2975eee347
Alexey <alexey-sh@users.noreply.github.com>
2019-11-08 21:57:37 +0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -4,9 +4,10 @@ const presets = [
     {
       targets: {
         edge: "17",
-        firefox: "60",
-        chrome: "67",
-        safari: "11.1",
+        firefox: "52",
+        chrome: "40",
+        safari: "9",
+        ie: "11"
       },
       useBuiltIns: "usage",
     },
